ce22e483a42cdfb21c819b2dd05954625fefe2de
Chris Williams <chris.p.williams.314@gmail.com>
2022-10-20 14:33:48 +0100

    successfully extracts possible sub-dataframes

--- csv_split.py
+++ csv_split.py
@@ -11,15 +11,46 @@ def convert_to_ints(series):
 
 # should get csv and headers
 
-def split_and_insert(header_csv_path, new_headers):
+def split_to_dataframes(header_csv_path, new_headers):
+    """
+    Arguments:
+        header_csv_path : String containing path to csv file
+        new_headers : list containing user-defined column headings (empty list if headers already present)
+    Returns:
+        df_list : list of dataframes ready for insertion to sql
+        foreign_keys : dict containing mappings of foreign keys between tables
+    """
     main_df = 0
     if new_headers:
         main_df = pd.read_csv(header_csv_path, names=new_headers)
     else:
         main_df = pd.read_csv(header_csv_path)
-    all_tables = [main_df]
     non_uniques_df = main_df.loc[:, [col for col in main_df if not main_df[col].is_unique]].copy()
     int_compare_df = non_uniques_df.transform(convert_to_ints)
+    new_col_list = list(non_uniques_df.columns)
+    tables = [main_df]
+    already_checked_cols = [] 
+    for col in new_col_list:
+        new_table_cols = [col]
+        already_checked_cols.append(col)
+        for new_col in new_col_list:
+            if new_col not in already_checked_cols:
+                if int_compare_df[new_col].compare(int_compare_df[col], keep_shape=True)["self"].all():
+                    new_table_cols.append(new_col)
+        if new_table_cols == [col]:
+            print("escape!")
+            continue
+        new_table = main_df.loc[:, new_table_cols].drop_duplicates()
+        tables.append(new_table)
+        print(new_col_list)
+    for table in tables:
+        print(table)
+        print("\n\n")
+
+
+
+        
+        
 
 
 
---
+++ my_tests.py
@@ -0,0 +1,3 @@
+import csv_split as c_splt
+
+c_splt.split_to_dataframes("C:/Users/ChrisWilliams/Documents/reader_test_csv.csv", [])
