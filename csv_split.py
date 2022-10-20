844b4dc00a218e4f8f670e30338bd2020bf6db32
Chris Williams <chris.p.williams.314@gmail.com>
2022-10-20 11:50:44 +0100

    converts to ints ready to spot paired columns

--- csv_split.py
+++ csv_split.py
@@ -1,22 +1,25 @@
 import pandas as pd
 
 def convert_to_ints(series):
+    """
+    We replace each unique value in the series with an int corresponding to order of appearance.
+    Then, when we compare later, columns whose unique values appear in the same order and
+    same frequencies will appear identical.
+    """
     replace_dict = {val : num for num, val in enumerate(series.unique())}
-    series.replace()
-
-
-
+    return series.replace(replace_dict)
 
 # should get csv and headers
 
 def split_and_insert(header_csv_path, new_headers):
     main_df = 0
     if new_headers:
-        main_df = pd.load_csv(header_csv_path, names=new_headers)
+        main_df = pd.read_csv(header_csv_path, names=new_headers)
     else:
-        main_df = pd.load_csv(header_csv_path)
+        main_df = pd.read_csv(header_csv_path)
     all_tables = [main_df]
-    non_uniques_df = main_df.loc[:, [col for col in main_df if not col.is_unique]].copy()
+    non_uniques_df = main_df.loc[:, [col for col in main_df if not main_df[col].is_unique]].copy()
+    int_compare_df = non_uniques_df.transform(convert_to_ints)
 
 
 
